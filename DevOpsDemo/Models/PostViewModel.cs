fb2d3dc84a6f78f4b35d9916d374bbaf061f6a06
Riya.Sankesara <riya.sankesra@ics-global.in>
2024-08-08 15:28:05 +0530

    Add project files.

---
+++ DevOpsDemo/Models/PostViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DevOpsDemo.Models
+{
+    public partial class PostViewModel
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+
+}
---
+++ DevOpsDemo/Repository/PostRepository.cs
@@ -0,0 +1,19 @@
+﻿using DevOpsDemo.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Numerics;
+
+namespace DevOpsDemo.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        public List<PostViewModel> GetPosts()
+        {
+            var posts = new List<PostViewModel> {
+            new PostViewModel(){ PostId =101, Title = "DevOps Demo Title 1", Description ="DevOps DemoDescription 1", Author="Riya Sankesara"},
+            new PostViewModel(){ PostId =102, Title = "DevOps Demo Title 2", Description ="DevOps DemoDescription 2", Author="Ritu Shah"},
+            new PostViewModel() { PostId = 103, Title = "DevOps Demo Title 3", Description = "DevOps DemoDescription 3", Author = "Darshil Shah" },
+            };
+            return posts;
+        }
+    }
+}
