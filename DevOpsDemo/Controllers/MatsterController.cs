e6ad99e327a77a19b0e5b815a1a4ffdbcd354cf0
Riya.Sankesara <riya.sankesra@ics-global.in>
2025-04-17 14:40:26 +0530

    added master controller

---
+++ DevOpsDemo/Controllers/MatsterController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsDemo.Controllers {
+    public class MatsterController : Controller {
+        // GET: MatsterController
+        public ActionResult Index() {
+            return View();
+        }
+
+        // GET: MatsterController/Details/5
+        public ActionResult Details(int id) {
+            return View();
+        }
+
+        // GET: MatsterController/Create
+        public ActionResult Create() {
+            return View();
+        }
+
+        // POST: MatsterController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: MatsterController/Edit/5
+        public ActionResult Edit(int id) {
+            return View();
+        }
+
+        // POST: MatsterController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+
+        // GET: MatsterController/Delete/5
+        public ActionResult Delete(int id) {
+            return View();
+        }
+
+        // POST: MatsterController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection) {
+            try {
+                return RedirectToAction(nameof(Index));
+            }
+            catch {
+                return View();
+            }
+        }
+    }
+}
